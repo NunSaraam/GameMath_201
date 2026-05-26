2efde238bcd8abf393398282700d46748f2b3fb9
NunSaraam <yjc22950037@gmail.com>
2026-05-26 15:21:27 +0900

    [Feat] 12강 과제 해야 함

---
+++ Assets/Scripts/12/ReflectAuto.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ReflectAuto : MonoBehaviour
+{
+    public Vector3 velocity = new Vector3(2f, -3f, 0f);
+
+    private void Update()
+    {
+        transform.position += velocity * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Vector3 normal = col.contacts[0].normal.normalized;
+
+        Vector3 reflect = Vector3.Reflect(velocity, normal);
+
+        velocity = reflect;
+    }
+}
